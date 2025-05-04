9a4a7b3345a03344625cb07aec5586af5f9592ed
Harsh Verma <harsh.verma@hp.com>
2025-05-05 01:52:19 +0530

    removing gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
-    base: "/spoonacular-enhanced",
     plugins: [react(), commonjs()],
     server: {
         port: 3000,
